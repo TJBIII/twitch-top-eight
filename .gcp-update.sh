597939a87ea1e8412cf6fd46513dd87d33a45839
Thomas Buida <thomas@gamewisp.com>
2017-09-30 12:50:37 -0500

    create deployment scripts. create ingress file and prod dockerfile.

---
+++ .gcp-update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+VERSION_NUMBER=$(date +%s)
+
+# build the image 
+docker build --no-cache -f Dockerfile-prod -t twitch-ext-top-eight:${VERSION_NUMBER} .
+
+docker tag twitch-ext-top-eight:${VERSION_NUMBER} us.gcr.io/twitch-extensions/twitch-ext-top-eight:${VERSION_NUMBER}
+
+gcloud docker -- push us.gcr.io/twitch-extensions/twitch-ext-top-eight:${VERSION_NUMBER}
+
+# connect to the cluster
+gcloud container clusters get-credentials top-eight-ext \
+  --zone us-central1-a --project twitch-extensions
+
+# apply a rolling update to the existing deployment with an image update
+kubectl set image deployment/top-eight-ext top-eight-ext=us.gcr.io/twitch-extensions/twitch-ext-top-eight:${VERSION_NUMBER}
+
+kubectl proxy --port=9061
